844f7906cb48ee11dcc1aad7f0b209d230097dff
Liam Brock <liam.brock99@gmail.com>
2021-01-29 12:49:39 +1000

    testing out sessions with login/register

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
+const morgan = require('morgan');
 const { register, login } = require('./routes/users');
 
 const app = express();
 
+app.use(morgan('dev'));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+
 app.use(express.json());
 
-app.get('/', (_req, res) => {
-  res.send('Hello :)');
+app.get('/', (req, res) => {
+  res.json(req.session);
 });
 
 app.use('/api/register', register);
 app.use('/api/login', login);
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(5000, () => console.log('Listening on port 5000'));
--- routes/users.js
+++ routes/users.js
@@ -11,11 +11,11 @@ async function register(req, res, next) {
       const count = await User.count({ where: { email } });
 
       if (count > 0) {
-        res.status(400).json({ message: 'Failed to register user: User already exists' });
+        res.status(400).json({ message: 'User already exists' });
       } else {
         const hash = await argon2.hash(password);
         User.create({ email, password: hash });
-        res.status(201).json({ message: 'Successfully registered user' });
+        res.status(201).json({ message: 'Registration successful' });
       }
     } catch (err) {
       console.log(err);
@@ -26,8 +26,33 @@ async function register(req, res, next) {
   }
 }
 
-function login(req, res) {
-  res.send('Hello /login');
+async function login(req, res, next) {
+  const { email, password } = req.body;
+
+  if (email && password) {
+    try {
+      // Try to find user with email
+      const user = User.findOne({ where: { email } });
+      if (user) {
+        // Compare passwords
+        const hash = await argon2.hash(password);
+        const userHash = user.password;
+        if (hash === userHash) {
+          // Store uid in session
+          console.log(`userid: ${user.id}`);
+          req.session.id = user.id;
+          res.status(201).json({ message: 'Login successful' });
+        } else {
+          res.status(400).json({ message: 'Incorrect password' });
+        }
+      } else {
+        res.status(400).json({ message: 'User not found' });
+      }
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  }
 }
 
 module.exports = {
